cfaf98e7f4733e1d394bb9c7dd14ffaa89893cdd
Nathan Crawford <njcrawford@gmail.com>
2008-03-24 14:53:24 +0000

    Added gpl2 info

--- PesFile/PesFile.cs
+++ PesFile/PesFile.cs
@@ -1,3 +1,27 @@
+/*
+Embridery Reader - an application to view .pes embroidery designs
+
+Copyright (C) 2008  Nathan Crawford
+ 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.
+
+A copy of the full GPL 2 license can be found in the docs directory.
+You can contact me at http://www.njcrawford.com/contact.html.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Text;
