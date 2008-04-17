81c2e6892a846e66f3e65e0f7f9f2ae76b326ef9
Nathan Crawford <njcrawford@gmail.com>
2008-04-17 02:54:36 +0000

    Added new findings for CSewFigSeg and added stub for recognizing CEmbCirc class.

--- PesFile/Properties/AssemblyInfo.cs
+++ PesFile/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.171")]
-[assembly: AssemblyFileVersion("1.0.1.171")]
+[assembly: AssemblyVersion("1.0.1.195")]
+[assembly: AssemblyFileVersion("1.0.1.195")]
