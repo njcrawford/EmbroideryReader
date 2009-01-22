ae3cf96758f3ba1deb31955f26ac8670e749dc42
Nathan Crawford <njcrawford@gmail.com>
2009-01-22 03:56:40 +0000

    Added shell thumbnail project, bumped version for next release

---
+++ embroideryThumbs/PESThumbnail.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace embroideryThumbs
+{
+    public class PESThumbnail 
+    {
+        //public HRESULT QueryInterface()
+        //{
+
+        //}
+        //public UInt64 AddRef()
+        //{
+
+        //}
+        //public UInt64 Release()
+        //{
+
+        //}
+        //public HRESULT Extract(HBITMAP
+    }
+}
